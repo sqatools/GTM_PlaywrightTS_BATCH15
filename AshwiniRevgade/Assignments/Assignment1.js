4043654241b3871b89b34f8943777dfdd8a3ec5e
AshwiniRevgade <ashwinirevgade701@gmail.com>
2026-07-10 20:22:44 +0530

    add file

---
+++ AshwiniRevgade/Assignments/Assignment1.js
@@ -0,0 +1,9 @@
+var base=10
+var base1=20
+var height=30
+var Diagonal1=40
+var Diagonal2=50
+console.log("Area of triangle is:",(base*height)/2)
+console.log("Perimeter of triangle is:", base+height+height)
+console.log("Area of Paralleleogram is:", base*height)
+console.log("Ar of Rhombus is:", Diagonal1*Diagonal2)
