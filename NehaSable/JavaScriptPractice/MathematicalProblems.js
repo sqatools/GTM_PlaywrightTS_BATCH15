41b904b5092473ba77bad1d5071d398eb9e733a9
Neha P Sable <nehasable1194@gmail.com>
2026-07-10 20:17:03 +0530

    adding assignment file

---
+++ NehaSable/JavaScriptPractice/MathematicalProblems.js
@@ -0,0 +1,76 @@
+//Area of Triangle
+var base=5;
+var height=10;
+var area=(base*height)/2;
+console.log("Area of Triangle is: "+area);
+//Perimeter of Triangle
+var side1=5;
+var side2=6;    
+var side3=7;
+var perimeter=side1+side2+side3;
+console.log("Perimeter of Triangle is: "+perimeter);
+//Area od Parallelogram
+var base=5;
+var height=10;      
+var area=(base*height);
+console.log("Area of Parallelogram is: "+area); 
+//Area of Rohmbus
+var diagonal1=5;
+var diagonal2=10;   
+var area=(diagonal1*diagonal2)/2;
+console.log("Area of Rhombus is: "+area);
+//Area of trapezium
+var base1=5;
+var base2=10;   
+var height=7;
+var area=((base1+base2)*height)/2;
+console.log("Area of Trapezium is: "+area);
+//Volume of cylinder
+var radius=5;
+var height=10;
+var volume=3.14*radius*radius*height;
+console.log("Volume of Cylinder is: "+volume);
+//Surface Area of Cylinder
+var radius=5;
+var height=10;
+var surfaceArea=2*3.14*radius*(radius+height);
+console.log("Surface Area of Cylinder is: "+surfaceArea);
+//Volume of cone
+var radius=5;
+var height=10;
+var volume=(1/3)*3.14*radius*radius*height;
+console.log("Volume of Cone is: "+volume);
+//Volume of Sphere
+var radius=5;
+var volume=(4/3)*3.14*radius*radius*radius;
+console.log("Volume of Sphere is: "+volume);
+//Surface Area of Sphere
+var radius=5;
+var surfaceArea=4*3.14*radius*radius;
+console.log("Surface Area of Sphere is: "+surfaceArea);
+//speed, distance and time
+var distance=100;
+var time=2;
+var speed=distance/time;
+console.log("Speed is: "+speed);
+//distance, speed and time
+var speed=50;
+var time=2; 
+var distance=speed*time;
+console.log("Distance is: "+distance);
+//time, speed and distance
+var distance=100;
+var speed=50;
+var time=distance/speed;
+console.log("Time is: "+time);
+//force, mass and acceleration
+var mass=10;
+var acceleration=5;
+var force=mass*acceleration;
+console.log("Force is: "+force);
+//work, force and distance
+var force=10;
+var distance=5;     
+var work=force*distance;
+console.log("Work is: "+work);
+
