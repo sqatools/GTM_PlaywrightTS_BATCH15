7c65e9e8c6d184cac092ca6f5242bc9f1a6ad389
DivyaG1987 <divyaphani.gvk@gmail.com>
2026-07-10 20:27:56 +0530

    added new files

---
+++ DivyaG/JavascriptPractice/JSVariable/JSOperators.js
@@ -0,0 +1,18 @@
+var x=2
+var y=3
+var z=4
+console.log("Area of triangle is",(x*y)/2)
+console.log("Perimeter of triangle is",x+y+z)
+console.log("Area of parallelogram:",x*y)
+console.log("Area of Rhombus:",(x*y)/2)
+console.log("Area of Trapezium:",((x+y)*z)/2)
+console.log("Volume of cylinder:",3.14*x*x*y)
+console.log("Surface Area of Cylinder",2*3.14*x*(x=y))
+console.log("Volume of cone:",(3.14*x*x*y)/3)
+console.log("Volume of Sphere:",(4/3)*3.14*x*x*x)
+console.log("Surface Area of Sphere:",4*3.14*x*x)
+console.log("Speed",x/y)
+console.log("Distance",x*y)
+console.log("Time",x/y)
+console.log("Force",x*y)
+console.log("Work",x*y)
