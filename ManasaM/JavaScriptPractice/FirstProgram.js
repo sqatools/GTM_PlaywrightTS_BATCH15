74cce0e0279f8dad065dbf10b029e2e40b6f9ba8
mansammpgit <mansammp@gmail.com>
2026-07-10 20:01:07 +0530

    adding file

---
+++ ManasaM/JavaScriptPractice/FirstProgram.js
@@ -0,0 +1,119 @@
+/*
+console.log("Hello World")
+
+
+
+ let P=100
+console.log("Value of P:",P)
+let Q = 200
+console.log("Value of Q:",Q)
+Q=300
+let Q=500
+console.log("Value of Q:",Q)
+
+var a=100
+console.log("Value of a:",a)
+var a=200
+console.log("Value of a:",a)
+
+var x=500
+if(true){
+    var x=100
+    let y=200
+    console.log("var x inside if block:",x)
+    console.log("let y inside if block:",y)
+
+}
+console.log("var x outside if block:",x)
+console.log("let y outside if block:",y)
+
+const z=100
+console.log("Value of z:",z)
+z=200
+console.log("Value of z:",z)
+
+
+var x =100
+var y =50
+console.log("Value of x:",x)
+console.log("Value of y:",y)
+console.log("Sum of x and y:",x+y)
+console.log("Subtraction of x and y:",x-y)
+console.log("Multiplication of x and y:",x*y)
+console.log("Division of x and y:",x/y) 
+console.log("Modulus of x and y:",20%3)
+console.log("Exponent of x and y:",5**3)
+
+*/
+// 
+var base= 10
+var height= 20
+// area= 0.5*base*height
+console.log("Area of triangle:",(base*height)/2)
+
+// Perimeter of triangle 
+var s1=2
+var s2=3
+var s3=4
+//var perimeter= s1+s2+s3
+console.log("Perimeter of triangle:",s1+s2+s3)
+
+//Area of parallelogram
+var base= 2
+var height = 3
+console.log("Area of parallelogram:",base*height)
+
+//Area of rectangle
+var length= 10
+var breadth= 20
+console.log("Area of rectangle:",length*breadth)        
+
+//Area of rhombus
+var d1= 10
+var d2= 20
+console.log("Area of rhombus:",(d1*d2)/2)
+
+//Area of trapezium
+var a= 10
+var b= 20
+var h= 30
+console.log("Area of trapezium:",((a+b)*h)/2)   
+
+//Volume of cylinder
+var r= 10
+var h= 20
+console.log("Volume of cylinder:",3.14*r*r*h)   
+//surface area of cylinder
+var r= 10
+var h= 20
+console.log("Surface area of cylinder:",2*3.14*r*(r+h)) 
+//Volume of cone
+var r= 10
+var h= 20
+console.log("Volume of cone:",(3.14*r*r*h)/3)   
+//volumne of sphere
+let r1= 10
+console.log("Volume of sphere:",(4/3)*3.14*r1*r1*r1)   
+//surface area of sphere
+let r2= 10
+console.log("Surface area of sphere:",4*3.14*r2*r2)   
+//Speed 
+let d=100
+let t=10
+console.log("Speed:",d/t)   
+//distance
+let s=100
+let t1=10
+console.log("Distance:",s*t)    
+//time
+let s1=100
+let d1=10
+console.log("Time:",d1/s1)    
+//force
+const m=100
+const a=10
+console.log("Force:",m*a)   
+//work done
+const f=100
+const d2=10
+console.log("Work done:",f*d2)   
