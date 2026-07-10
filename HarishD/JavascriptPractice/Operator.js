82d6447a59a0f6722905d8ccccf828594299200f
deshpandeharish328 <deshpandeharish328@gmail.com>
2026-07-10 20:53:45 +0530

    adding file

--- HarishD/JavascriptPractice/Operator.js
+++ HarishD/JavascriptPractice/Operator.js
@@ -11,3 +11,12 @@ var x= 100
 var y = 50
 
 console.log("addition of two number :" , x+y)
+console.log("substarction of two numbers :", x-y)
+console.log("multiplication of x and y is :",x*y )
+console.log("divison of x and y is :", x/y )
+
+//addi
+
+var base = 20
+var height = 100
+
