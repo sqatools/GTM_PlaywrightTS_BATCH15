405f8663a9f9cb463f73fdcad339fab6c8715d54
Vishwanatha <vishwashettyy>
2026-07-10 23:04:40 +0530

    10th July class notes and assignment operators

---
+++ Vishwashetty/JavaScriptPractice/Assignment_1.js
@@ -0,0 +1,210 @@
+console.log("Write a pragram to print area of triangle using base and height")
+var height = 100
+var Base = 50
+var Area = (Base * height) / 2
+console.log("Area of Triangle :", Area)
+
+console.log("Write a pragram to print perimeter of triangle using side1, side2 and side3")
+var side1 = 100
+var side2 = 50
+var side3 = 30
+var perimeter = side1 + side2 + side3
+console.log("Perimeter of Triangle :", perimeter)
+
+console.log("Write a pragram to print area of parallelogram using base and height")
+var base = 100
+var height = 50
+var area = base * height
+console.log("Area of Parallelogram :", area)    
+
+console.log("Write a pragram to print area of rhombus using diagonal1 and diagonal2")
+var diagonal1 = 100
+var diagonal2 = 50
+var area = (diagonal1 * diagonal2) / 2
+console.log("Area of Rhombus :", area)
+
+console.log("Write a pragram to print area of trapezium using base1, base2 and height")
+var base1 = 100
+var base2 = 50
+var height = 30
+var area = ((base1 + base2) * height) / 2
+console.log("Area of Trapezium :", area)
+
+console.log("Write a pragram to print volume of cylinder using radius and height")
+var radius = 100
+var height = 50
+var volume = Math.PI * radius**2 * height     // Math.PI * Math.pow(radius, 2) * height
+console.log("Volume of Cylinder :", volume)
+
+console.log("Write a pragram to print surface area of cylinder using radius and height")
+var radius = 100
+var height = 50
+var surfaceArea = 2 * Math.PI * radius * (radius + height)
+console.log("Surface Area of Cylinder :", surfaceArea)
+
+console.log("Write a pragram to print volume of cone using radius and height")
+var radius = 100
+var height = 50
+var volume = (Math.PI * Math.pow(radius, 2) * height) / 3
+console.log("Volume of Cone :", volume) 
+
+console.log("Write a pragram to print volume of sphere using radius")
+var radius = 100
+var volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+console.log("Volume of Sphere :", volume)
+
+console.log("Write a pragram to print surface area of sphere using radius")
+var radius = 100
+var surfaceArea = 4 * Math.PI * Math.pow(radius, 2)
+console.log("Surface Area of Sphere :", surfaceArea)    
+
+console.log("Write a pragram to print speed using distance and time")
+var distance = 100
+var time = 50
+var speed = distance / time
+console.log("Speed :", speed)
+
+console.log("Write a pragram to print distance using speed and time")
+var speed = 100
+var time = 50
+var distance = speed * time
+console.log("Distance :", distance) 
+
+console.log("Write a pragram to print time using distance and speed")
+var distance = 100
+var speed = 50
+var time = distance / speed
+console.log("Time :", time)
+
+console.log("Write a pragram to print force using mass and acceleration")
+var mass = 100
+var acceleration = 50
+var force = mass * acceleration
+console.log("Force :", force)
+
+console.log("Write a pragram to print work done using force and distance")
+var force = 100
+var distance = 50
+var workDone = force * distance
+console.log("Work Done :", workDone)
+
+console.log("Write a pragram to print power using work and time")
+var work = 100
+var time = 50
+var power = work / time
+console.log("Power :", power)
+
+console.log("Write a pragram to print kinetic energy using mass and velocity")
+var mass = 10
+var velocity = 5
+var kineticEnergy = (1 / 2) * mass * velocity**2 //Math.pow(velocity, 2)
+console.log("Kinetic Energy :", kineticEnergy)
+
+console.log("Write a pragram to print potential energy using mass, gravity and height")
+var mass = 100
+var gravity = 9.8
+var height = 50
+var potentialEnergy = mass * gravity * height
+console.log("Potential Energy :", potentialEnergy)
+
+console.log("Write a pragram to print momentum using mass and velocity")
+var mass = 100
+var velocity = 50
+var momentum = mass * velocity
+console.log("Momentum :", momentum)
+
+console.log("Write a pragram to print pressure using force and area")
+var force = 100
+var area = 50
+var pressure = force / area
+console.log("Pressure :", pressure) 
+
+console.log("Write a pragram to print density using mass and volume")
+var mass = 100
+var volume = 50
+var density = mass / volume
+console.log("Density :", density)
+
+console.log("Write a pragram to print simple interest using principal, rate and time")
+var principal = 100
+var rate = 5
+var time = 2
+var simpleInterest = (principal * rate * time) / 100
+console.log("Simple Interest :", simpleInterest)
+
+console.log("Write a pragram to print compound interest using principal, rate and time")
+var principal = 100
+var rate = 5
+var time = 2
+var compoundInterest = principal * (1 + rate / 100) ** time - principal
+console.log("Compound Interest :", compoundInterest)
+
+console.log("Write a pragram to print total amount using principal and simple interest")
+var principal = 100
+var simpleInterest = 10
+var totalAmount = principal + simpleInterest
+console.log("Total Amount :", totalAmount)  
+
+console.log("Write a pragram to print discount amount using price and discount percentage")
+var price = 100
+var discountPercentage = 10
+var discountAmount = (price * discountPercentage) / 100
+console.log("Discount Amount :", discountAmount)    
+
+console.log("Write a pragram to print final price using price and discount")
+var price = 100
+var discount = 10
+var finalPrice = price - discount
+console.log("Final Price :", finalPrice)
+
+console.log("Write a pragram to print GST amount using price and GST percentage")
+var price = 100
+var gstPercentage = 18
+var gstAmount = (price * gstPercentage) / 100
+console.log("GST Amount :", gstAmount)  
+
+console.log("Write a pragram to print total bill with GST using price and GST")
+var price = 100
+var gst = 18
+var totalBillWithGST = price + gst
+console.log("Total Bill with GST :", totalBillWithGST)
+
+console.log("Write a pragram to print profit using selling price and cost price")
+var sellingPrice = 100
+var costPrice = 50
+var profit = sellingPrice - costPrice
+console.log("Profit :", profit)
+
+console.log("Write a pragram to print loss using cost price and selling price")
+var costPrice = 100
+var sellingPrice = 50
+var loss = costPrice - sellingPrice
+console.log("Loss :", loss)     
+
+console.log("Write a pragram to print profit percentage using profit and cost price")
+var profit = 50
+var costPrice = 100
+var profitPercentage = (profit / costPrice) * 100
+console.log("Profit Percentage :", profitPercentage)    
+
+console.log("Write a pragram to print loss percentage using loss and cost price")
+var loss = 50
+var costPrice = 100
+var lossPercentage = (loss / costPrice) * 100
+console.log("Loss Percentage :", lossPercentage)    
+
+console.log("Write a pragram to print percentage using obtained and total")
+var obtained = 50
+var total = 100
+var percentage = (obtained / total) * 100
+console.log("Percentage :", percentage)
+
+console.log("Write a pragram to print average of 5 numbers using a, b, c, d and e")
+var a = 10
+var b = 20
+var c = 30
+var d = 40
+var e = 50
+var average = (a + b + c + d + e) / 5
+console.log("Average :", average)   
+
