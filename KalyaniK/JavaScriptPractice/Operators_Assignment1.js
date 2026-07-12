2e345cf881a99ce653e7e1fade23a1f0891fd848
KRKalyani <rkalyanikandiraju@gmail.com>
2026-07-12 19:04:22 +0530

    adding two files

---
+++ KalyaniK/JavaScriptPractice/Operators_Assignment1.js
@@ -0,0 +1,34 @@
+
+//Traingle- Area
+{var base=10
+var height=5
+var area=base*height
+console.log("Area", area)}
+//Triangle=Perimeter
+{
+ let side1=3
+let side2=4
+let side3=5
+Perimeter=side1+side2+side3
+}
+console.log("Perimeter", Perimeter)
+
+// area of a parallelogram
+var base = 10;
+var height = 5;
+var area = base * height;
+console.log("Area of Parallelogram", area);
+
+//Area of rhombus
+var diagonal1 = 8;
+var diagonal2 = 6;
+Area = (diagonal1 * diagonal2) / 2;
+console.log("Area of Rhombus", Area);
+
+//Area of trapezium
+var B1, B2
+B1=10;
+B2=5;
+let H=3
+Area=((B1+B2)*H)/2
+console.log("Area of Trapezium =",Area)
---
+++ KalyaniK/JavaScriptPractice/operators.js
@@ -0,0 +1,10 @@
+{var base=10
+var height=5
+var area=base*height
+console.log("Area", area)}
+var side1=3
+var side2=4
+var side3=5
+Perimeter=side1+side2+side3
+console.log("Perimeter", Perimeter)
+var Rad=7
