ccf5ee6d01c66b6eea641be3c960b1de3f0aecc6
DivyaG1987 <divyaphani.gvk@gmail.com>
2026-07-14 18:48:51 +0530

    files added

---
+++ DivyaG/JavascriptPractice/DataTypes/dataType.js
@@ -0,0 +1,23 @@
+//Primitive data type
+
+//1.number
+n1 = 0
+n2 = 8.9
+n3 = 7890
+n4 = 788999999999999
+n5 = -98989
+console.log(n1,":", typeof n1)
+console.log(n2,":", typeof n2)
+console.log(n3,":", typeof n3)
+console.log(n4,":", typeof n4)
+console.log(n5,":", typeof n5)
+
+//2.String
+var s1="d"
+var s2="divya"
+var s3="divya is best"
+var s4="23"
+console.log("s1 :", s1 ,":",typeof(s1))
+console.log("s2 :", s2 ,":",typeof(s2))
+console.log("s3 :", s3,":",typeof(s3))
+console.log("s4 :", s4,":",typeof(s4))
