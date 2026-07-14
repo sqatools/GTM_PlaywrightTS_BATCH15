d7eb1134b46c65c54b496aec3e6e6cef50b2e591
Dnyaneshwar007 <dnyaneshwar.dandale007@gmail.com>
2026-07-14 14:56:06 +0530

    added now file to repo

--- Dnyaneshwar/Test/JSOperator.js
+++ Dnyaneshwar/Test/JSOperator.js
@@ -25,3 +25,46 @@ console.log("Division  of x,y :",x/y)
 console.log("Modulus operator of x,y :",x%y)
 
 console.log("Squre of number :",2**3);
+
+
+console.log(".........Assignment Operator.......")
+
+//1. = : Assignment operator
+
+var x=50;
+
+console.log("value of x is : ",x)
+
+//2. += : add and assign operator
+
+x +=10;
+
+console.log("value of x is : x +=10 ",x)
+
+// 3. -= : subtract and assign operator
+
+x -=10;
+
+console.log("value of x is : x -=10: ",x)
+
+
+//4. *= : multiply and assign operator
+
+x *=10;
+
+console.log("value of x is : x *=10 :",x)   
+
+//5. /= : divide and assign operator
+
+x /=10;     
+console.log("value of x is : x /=10 :",x)   
+
+//6. %= : modulus and assign operator
+
+x %=10;     
+console.log("value of x is : x %=10 :",x) 
+
+//7. **= : exponentiation and assign operator
+
+x **=2;     
+console.log("value of x is : x **=10 :",x)  
