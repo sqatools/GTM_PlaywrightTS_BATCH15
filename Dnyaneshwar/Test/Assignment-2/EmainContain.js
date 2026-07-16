44445a1791c311d3d8ed7aa66ccfd870c25aa171
Dnyaneshwar007 <dnyaneshwar.dandale007@gmail.com>
2026-07-16 15:47:52 +0530

    added assignment2 to repo

---
+++ Dnyaneshwar/Test/Assignment-2/EmainContain.js
@@ -0,0 +1,10 @@
+
+var email= "abc@gmail.com"
+
+if(email = "@"){
+    console.log("Email contains @ symbol")
+}
+else
+{
+    console.log("Email does not contains @ symbol")
+}
---
+++ Dnyaneshwar/Test/Assignment-2/NumberDivisibleBy2or3.js
@@ -0,0 +1,10 @@
+var num =31;
+
+if(num %2==0 || num %3==0)
+{
+    console.log("number is divisible by 2 or 3")
+}
+else
+{
+    console.log("number is not divisible by 2 or 3")
+}
