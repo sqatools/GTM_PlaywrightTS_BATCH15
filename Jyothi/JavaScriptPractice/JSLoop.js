d64735b3536d0f3f275a25b3d74fb6be5034563c
Jyothi Chenikala <chenikalajyothi@gmail.com>
2026-07-18 00:31:40 +0530

    added

---
+++ Jyothi/JavaScriptPractice/JSLoop.js
@@ -0,0 +1,18 @@
+//For Loop
+
+for(var i=0;i<=10;i++)
+
+{
+    console.log(i)
+}
+
+//Apply for loop with reverse values
+
+for(var i=10;i>=1;i--)
+{
+    console.log(i)
+
+}
+
+//table of given value
+var n4=3
