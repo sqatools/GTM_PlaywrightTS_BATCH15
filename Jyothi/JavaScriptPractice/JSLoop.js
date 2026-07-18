458896fcf56bf7b71ba00944b01d30b9ee56cfe3
Jyothi Chenikala <chenikalajyothi@gmail.com>
2026-07-18 08:44:45 +0530

    added

--- Jyothi/JavaScriptPractice/JSLoop.js
+++ Jyothi/JavaScriptPractice/JSLoop.js
@@ -16,3 +16,18 @@ for(var i=10;i>=1;i--)
 
 //table of given value
 var n4=3
+for(i=1;i<=5;i++)
+{
+   console.log(i,"*",n4 ,":",i*n4)
+}
+
+
+//Get factorial of given values
+
+var fact=1
+for(var i=5;i>=1;i--)
+{
+    fact*=i
+}
+console.log("factorial output",fact)
+
