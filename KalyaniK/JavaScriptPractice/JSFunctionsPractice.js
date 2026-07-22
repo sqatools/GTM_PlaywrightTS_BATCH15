1c5c002b6ff553a1869b39e2f0f5614f19644027
KRKalyani <rkalyanikandiraju@gmail.com>
2026-07-22 20:49:01 +0530

    adding files

---
+++ KalyaniK/JavaScriptPractice/JSFunctionsPractice.js
@@ -0,0 +1,11 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+function getEvenValuse(arr){
+for(var val of arr)
+    {
+    if((val%2)==0)
+        {
+        console.log()
+        }
+    }
+}
+    getEvenValuse(arr)
