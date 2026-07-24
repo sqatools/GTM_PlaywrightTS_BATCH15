591337465f86360251a49197362658d0bbadce84
Dnyaneshwar-007 <dnyaneshwar.dandale007@gmail.com>
2026-07-24 13:33:38 +0530

    adding code

---
+++ Dnyaneshwar/Test/module2/callfunction.js
@@ -0,0 +1,15 @@
+import {Addition,Multiplication,SqureofNumber ,Getfactorial} from '../Module1/math.js'
+
+ var Result=Addition(10,20);
+
+console.log("Addition of two number is : "+Result);
+
+var MulResult = Multiplication(10,20);
+console.log("Multiplication of two number is : "+MulResult);
+
+var SquareResult = SqureofNumber(15);
+console.log("Square of the number is : "+SquareResult);
+
+
+var factorial=Getfactorial(5);
+console.log("Factorial of the number is : "+factorial);
