dffea1d0021a9e11762b34023c0d745d1074cf55
deshpandeharish328 <deshpandeharish328@gmail.com>
2026-07-24 18:50:29 +0530

    import concept

---
+++ HarishD/JavascriptPractice/JsFinction/module1/math_Operation.js
@@ -0,0 +1,18 @@
+ export function addition(num1 , num2)
+{
+    return num1+num2
+}
+
+ export function getSquare(num)
+{
+    return num**2
+}
+
+ export function fact (num)
+{
+    fact =1
+    for(var i =num;i>0; i--){
+        fact*=i
+    }
+    return fact
+}
