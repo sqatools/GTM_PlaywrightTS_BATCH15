549149a94fdef3c7a19b82e13562f91072f5cfe3
Vishwanatha <vishwashettyy>
2026-07-26 23:06:12 +0530

    condition practise

---
+++ Vishwashetty/JavaScriptPractice/d_JSconditions/Conditionpractise.js
@@ -0,0 +1,85 @@
+console.log("###################################")
+var num = 20
+console.log(num)
+
+if(num%2==0){
+    console.log("Evenn")
+
+}else{
+    console.log("Odd")
+}
+
+console.log("###################################")
+
+var num = 30
+if(num%3==0&&num%5==0){
+console.log('yes')
+}else{
+    console.log('no')
+}
+
+console.log("###################################")
+var num = 18
+if (num%3==0 || num%5==0)
+{
+    console.log('yes')
+}else{
+    console.log('no')
+}
+
+console.log("###################################")
+
+var num1 = 50
+var num2 = 303
+var num3 = 40
+if (num1>num2 && num1>num3){
+    console.log('num1 is greater')
+}
+else if (num2>num1&&num2>num3){
+    console.log('num2 is graeter')
+}
+else if(num3>num1&&num3>num2){
+    console.log('num3 is greater')
+}
+else {
+    console.log('no number is greater')
+}
+
+console.log("###################################")
+
+var round1 = 'pass'
+var round2 = 'pass'
+var round3 = 'pass'
+
+if(round1=='pass'){
+    console.log('round one is cleared')
+if(round2=='pass'){
+console.log('round two is cleared')
+
+                if(round3=='pass'){
+                 console.log('round 3 is cleared')
+                              }else{
+                                     console.log('rejected in round 3')
+                                                 }
+            }else{
+    console.log('rejected in round 2')
+                }
+}else{
+    console.log('rejected in round 3')
+}
+
+console.log("###################################")
+
+
+var num = 78 
+result = num%2==0? 'even': 'odd'
+console.log(result)
+
+console.log("###################################")
+
+var num1 = 10
+var num2 = 10
+num = num1==num2 ? 'equal':'not equal'
+console.log(num) 
+
+console.log("###################################")
