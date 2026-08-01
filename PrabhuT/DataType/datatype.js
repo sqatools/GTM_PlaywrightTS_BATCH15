83e7cc22e5df4b0a6a15711b86d000f6afc0eebf
PrabhuAutomation <prabhutesting30.t@gmail.com>
2026-08-01 14:10:37 +0530

    adding the filr

---
+++ PrabhuT/DataType/datatype.js
@@ -0,0 +1,44 @@
+var n=34566
+var n1=23.45
+console.log (n)
+console.log (n1)
+
+//string
+
+var s1="Hello"
+console.log(s1[2])
+
+//undefined 
+
+var x 
+console.log(x)
+
+var y=null
+console.log(y)
+console.log (n1, typeof n1)
+
+//array
+
+var a1=[30,45,`javascript`,[5,7,8],{a:123,b:234},true]
+console.log(a1[3])
+
+// add new value in array 
+a1.push(100)
+console.log(a1)
+
+//object
+var b1={Name : "prabhu", age :25 }
+console.log(b1)
+
+b1.add="Pune"
+console.log(b1)
+
+//function
+
+function addition (n1,n2)
+{
+    console.log(n1+n2)
+
+}
+
+addition (30,40)
