f194d368082874355e0d18b35b5e811ad854cc49
RupaSMore <rupalimore656@gmail.com>
2026-08-06 20:31:38 +0530

    new files added

---
+++ Rupali/JavaScriptPractice/JSObjects/JSObjects.js
@@ -0,0 +1,13 @@
+var object1 = {
+    name : "Rahul",
+    age : 25,
+    email : 'rahul@gmail.com',
+    phone: 678687678,
+    hobbies : { game: 'cricket', music: 'rocking songs', food: 'healthy food'},
+    precitis : ['Pune', 'Bangalore', 'Mumbai'],
+    v1 : 4.5,
+    married : true,
+    age: 35
+
+}
+console.log(object1)
