fb54c30100ddb095ecd628c2cf876f952bb8438e
Nayana <nayanachide@gmail.com>
2026-08-26 17:20:33 +0530

    new file

--- NainaC/PlaywrightAutomation/tests/First_test_file.spec.ts
+++ NainaC/PlaywrightAutomation/tests/First_test_file.spec.ts
@@ -2,7 +2,7 @@ import {test} from '@playwright/test'
 test("Enter personal details dummy website", async ({page}) => {
 
 
-await page.goto("https//sqatools.in/dummy-booking-website/")
+await page.goto("https://sqatools.in/dummy-booking-website/")
 await page.locator("#firstname").nth(0).fill("Rahul")
 await page.locator("#firstname").nth(1).fill("Gupta")
 await page.locator("#male").check()
@@ -13,4 +13,13 @@ test("Enter the destination city", async({page}) => {
     await page.goto("https//sqatools.in/dummy-booking-website/")
     await page.locator("from city").fill("Indore")
     await page.locator("#density").fill("mumbai")
+});
+
+
+test("Search the product on amazon", async({page}) => {
+await page.goto("https://www.amazon.in/")
+await page.goto('https://www.amazon.in/b?node=14351766031');
+await page.getByRole('link', { name: 'Sell', exact: true }).click();
+await page.getByRole('link', { name: 'Sell', exact: true }).click();
+await page.getByRole('link', { name: 'Bestsellers' }).click();
 });
