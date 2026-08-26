8d99138aa9c0882fef26843877480ef7c053d700
sqatools <sqatools.automation@gmail.com>
2026-08-26 20:00:13 +0530

    added new code

--- DeepeshY/PlaywrightAutomation/tests/PlaywrightLocators/locators_methods.spec.ts
+++ DeepeshY/PlaywrightAutomation/tests/PlaywrightLocators/locators_methods.spec.ts
@@ -40,10 +40,38 @@ test.describe("Playwright Locators Methods", ()=> {
         await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
         await page.getByLabel("Email Address:").fill("user1@gmail.com")
         await page.getByLabel("Password: ").fill("test@12345")
+        await page.getByLabel("Your Age:").fill("35")
+        await page.getByLabel("Standard").check()
+        await page.getByLabel(" Express").check()
+
         
     });
 
+    test("getByPlaceholder() method", async({page})=> {
+        //<input type="text" placeholder="Enter your full name" class="full-width">
+         await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+         await page.getByPlaceholder("Enter your full name").fill("Rahul Sharma")
+         await page.getByPlaceholder("Type your message here...").fill("Lerning PLAYWRGHHT")
+
+    });
 
 
+     test("getByTestID, title() method", async({page})=> {
+        //<input type="text" placeholder="Enter your full name" class="full-width">
+         await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+         await page.getByAltText("logo image").isVisible()
+         await page.getByPlaceholder("Type your message here...").fill("Lerning PLAYWRGHHT")
 
-})
+         // get element by title
+         const textValue = await page.getByTitle("Home page link").textContent()
+         console.log(textValue)
+
+         // identiyfy elelmentus test id
+         console.log(await page.getByTestId("profile-email").textContent())
+         console.log(await page.getByTestId("profile-name").textContent())
+          console.log(await page.getByTestId("product-card-2").textContent())
+
+    });
+   
+
+});
