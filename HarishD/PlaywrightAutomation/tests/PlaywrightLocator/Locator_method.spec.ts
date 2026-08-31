65331b94a6868cd301abaac7e5e8601d7bd00aa7
deshpandeharish328 <deshpandeharish328@gmail.com>
2026-08-31 14:09:37 +0530

    locators

--- HarishD/PlaywrightAutomation/tests/PlaywrightLocator/Locator_method.spec.ts
+++ HarishD/PlaywrightAutomation/tests/PlaywrightLocator/Locator_method.spec.ts
@@ -1,4 +1,4 @@
-/*
+/* 
 page.getByRole() to locate by explicit and implicit accessibility attributes.
 page.getByText() to locate by text content.
 page.getByLabel() to locate a form control by associated label's text.
@@ -9,15 +9,65 @@ page.getByTestId() to locate an element based on its data-testid attribute (othe
 
 */
 
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test.describe("Playwright Locators Methods", ()=> {
+test.describe("Playwright Locators Methods", () => {
 
-    test("getByRole Method", async({page})=> {
+    test("getByRole Method", async ({ page }) => {
         await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
-        const Prbutton = page.getByRole("button", {name: "Primary Action"})
+        const Prbutton = page.getByRole("button", { name: "Primary Action" })
         // assertion and verify button status.
         await expect(Prbutton).toBeVisible()
 
+        await page.waitForTimeout(2000)
+
+        const Tbutton = page.getByRole("button", { name: "Toggle Button" })
+        // assertion and verify button status.
+        await expect(Tbutton).toBeVisible()
+
+        const CheckB = page.getByRole("checkbox", { name: " Accept terms" })
+        // assertion and verify button status.
+        await expect(CheckB).toBeVisible()
+        await page.getByRole("textbox", { name: "Username:" }).fill("Harish Deshpande")
+        await page.getByRole("checkbox", { name: "Accept terms" }).check()
+        await page.waitForTimeout(2000)
+        const menuitem = await page.getByRole("menuitem", { name: "Products" }).textContent()
+        console.log(menuitem)
+
     })
-})
+
+    test("getByText Method", async ({ page }) => {
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+        const startButton = page.getByText("START")
+        await expect(startButton).toBeVisible()
+        await expect(startButton).toBeEnabled()
+        await expect(startButton).toHaveText("START")
+        await expect(startButton).toHaveCount(1)
+        await expect(startButton).not.toBeDisabled()
+
+    })
+
+    test("getByLabel() Method", async({page})=> {
+            //<label for="email">Email Address:</label>
+            // we can user getByLabel method when label is tagname or it could be attrribute ass well
+    await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+
+    await page.getByLabel("Email Address:").fill("User133@gmail.com")
+    await page.getByLabel(" Password:").fill("abc@123")
+    await page.getByLabel("Your Age:").fill('24')
+    await page.getByLabel(" Standard").check()
+    });
+
+
+    test("getByPlaceholder() method", async({page})=> {
+            //<input type="text" placeholder="Enter your full name" class="full-width">
+             await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+             await page.getByPlaceholder("Enter your full name").fill("Harish Deshpande")
+             await page.getByPlaceholder("Phone number (xxx-xxx-xxxx)").fill("1234567890")
+             await page.getByPlaceholder("Type your message here...").fill("Lerning PLAYWRGHHT")
+             await page.getByPlaceholder("Search products...").fill("Shoes")
+    
+        });
+          
+
+});
