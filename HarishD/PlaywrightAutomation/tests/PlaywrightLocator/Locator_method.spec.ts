070377ff1fb26bf6b54e47f10a7e3673cc48c744
deshpandeharish328 <deshpandeharish328@gmail.com>
2026-08-31 19:46:22 +0530

    new file

--- HarishD/PlaywrightAutomation/tests/PlaywrightLocator/Locator_method.spec.ts
+++ HarishD/PlaywrightAutomation/tests/PlaywrightLocator/Locator_method.spec.ts
@@ -47,27 +47,46 @@ test.describe("Playwright Locators Methods", () => {
 
     })
 
-    test("getByLabel() Method", async({page})=> {
-            //<label for="email">Email Address:</label>
-            // we can user getByLabel method when label is tagname or it could be attrribute ass well
-    await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+    test("getByLabel() Method", async ({ page }) => {
+        //<label for="email">Email Address:</label>
+        // we can user getByLabel method when label is tagname or it could be attrribute ass well
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
 
-    await page.getByLabel("Email Address:").fill("User133@gmail.com")
-    await page.getByLabel(" Password:").fill("abc@123")
-    await page.getByLabel("Your Age:").fill('24')
-    await page.getByLabel(" Standard").check()
+        await page.getByLabel("Email Address:").fill("User133@gmail.com")
+        await page.getByLabel(" Password:").fill("abc@123")
+        await page.getByLabel("Your Age:").fill('24')
+        await page.getByLabel(" Standard").check()
     });
 
 
-    test("getByPlaceholder() method", async({page})=> {
-            //<input type="text" placeholder="Enter your full name" class="full-width">
-             await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
-             await page.getByPlaceholder("Enter your full name").fill("Harish Deshpande")
-             await page.getByPlaceholder("Phone number (xxx-xxx-xxxx)").fill("1234567890")
-             await page.getByPlaceholder("Type your message here...").fill("Lerning PLAYWRGHHT")
-             await page.getByPlaceholder("Search products...").fill("Shoes")
-    
-        });
-          
+    test("getByPlaceholder method", async ({ page }) => {
+        //<input type="text" placeholder="Enter your full name" class="full-width">
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+        await page.getByPlaceholder("Enter your full name").fill("Harish Deshpande")
+        await page.getByPlaceholder("Phone number (xxx-xxx-xxxx)").fill("1234567890")
+        await page.getByPlaceholder("Type your message here...").fill("Lerning PLAYWRGHHT")
+        await page.getByPlaceholder("Search products...").fill("Shoes")
 
-});
+    });
+
+    test("getByAltText method", async ({ page }) => {
+        //<input type="text" placeholder="Enter your full name" class="full-width">
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+        await page.getByAltText("logo image").isVisible()
+
+    });
+
+    test("getByTitle method", async ({ page }) => {
+        //<input type="text" placeholder="Enter your full name" class="full-width">
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html") 
+
+        const textvalue = await page.getByTitle("Home page link").textContent()
+        console.log(textvalue)          
+
+        // identiyfy elelmentus test id
+        console.log(await page.getByTestId("profile-email").textContent())
+        console.log(await page.getByTestId("profile-name").textContent())
+        console.log(await page.getByTestId("product-card-2").textContent())
+ });
+
+}); 
