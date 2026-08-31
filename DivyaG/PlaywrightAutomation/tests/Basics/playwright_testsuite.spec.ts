f3b0842570e3a938e6c0de036b98427e0aa23f61
DivyaG1987 <divyaphani.gvk@gmail.com>
2026-08-31 18:30:26 +0530

    added locators

---
+++ DivyaG/PlaywrightAutomation/tests/Basics/playwright_testsuite.spec.ts
@@ -0,0 +1,30 @@
+import {test} from '@playwright/test'
+test.describe("Booking website",()=>{
+    test("Flight Test1",()=>{
+        console.log("Flight Testcase1")
+    });
+
+     test("Flight Test2",()=>{
+        console.log("Flight Testcase2")
+    });
+
+     test("Hotel booking Test1",()=>{
+        console.log("Hotel booking Testcase1")
+    });
+
+    
+     test("Hotel booking Test2",()=>{
+        console.log("Hotel booking Testcase2")
+    });
+
+    
+     test("car booking Test1",()=>{
+        console.log("car booking Testcase1")
+    });
+    
+    test("car booking Test2",()=>{
+        console.log("car booking Testcase2")
+    });
+ 
+
+})
