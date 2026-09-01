2f11fbf8d3df47f7d151372825e6c1136a2f8b29
DivyaG1987 <divyaphani.gvk@gmail.com>
2026-09-01 20:16:01 +0530

    added

--- DivyaG/PlaywrightAutomation/tests/Basics/xpath_css_exmples.spec.ts
+++ DivyaG/PlaywrightAutomation/tests/Basics/xpath_css_exmples.spec.ts
@@ -12,8 +12,8 @@ test("select all check boxes using advance xpath",async({page})=>{
 
 test("Top colleges in India",async({page})=>{
     await page.goto("https://www.nirfindia.org/Rankings/2025/collegeRanking.html")
-    const allRows = await page.locator(`//table[@id='tbl_overall']/tbody/tr`).all
-    for(var i=1;i<allRows.length;i++){
+    const allRows = await page.locator(`//table[@id='tbl_overall']/tbody/tr`).all()
+    for(var i=1; i<=allRows.length; i++){
         const collegeDetails= await page.locator(`//table[@id='tbl_overall']/tbody/tr[${i}]/td[2]`).textContent()
     console.log(i,collegeDetails)
     console.log("------------------------")
