7d66f754c9586e6c6d5724671a02f477d29d2966
DivyaG1987 <divyaphani.gvk@gmail.com>
2026-09-07 15:29:25 +0530

    added

--- DivyaG/PlaywrightAutomation/tests/Basics/PlaywrightActions/playwrightActions.spec.ts
+++ DivyaG/PlaywrightAutomation/tests/Basics/PlaywrightActions/playwrightActions.spec.ts
@@ -33,5 +33,61 @@ test.describe("Playwright Actions", async()=>{
         })
     })
 
+    test("Select radio and checkbox types", async({page})=>{
+    await page.goto("https://sqatools.in/automation-practice-page/")
+    const radiobtn= await page.getByRole("radio",{name: "Male", exact: true})
+    await expect(radiobtn).not.toBeChecked();
+    await radiobtn.check()
+    await expect(radiobtn).toBeChecked();
+    
+    const checkboxbtn = await page.getByRole("checkbox",{name: "Java"})
+    await expect(checkboxbtn).not.toBeChecked();
+    await checkboxbtn.check();
+    await expect(checkboxbtn).toBeChecked();
+    })
+
+    test("Handling dropdown", async({page})=>{
+    await page.goto("https://sqatools.in/automation-practice-page/")
+    const countryDD= page.locator("#country")
+    await countryDD.scrollIntoViewIfNeeded();
+    await countryDD.selectOption("usa");
+    await page.waitForTimeout(3_000);
+    await countryDD.selectOption({label:"India"});
+//select multiple dropdown
+   const skillsdd = page.locator("#skills")
+   await skillsdd.selectOption(['Python','Selenium','Playwright']);
+})
+
+    test("Perform click options", async({page})=>{
+        await page.setViewportSize({width:2000,height:1080})
+        await page.goto("https://sqatools.in/automation-practice-page/");
+        const rightClickBtn = page.getByRole("button", {name: 'Normal Button'})
+        await rightClickBtn.click({'button':'right'})
+
+        await test.step("Control operations", async()=>{
+         const dummyPagectrl = page.getByRole("link",{name:'Dummy Page'}).first();
+         await dummyPagectrl.click({modifiers:['Control']})
+         await page.waitForTimeout(3_000)   
+        
+         await test.step("hover checking", async()=>{
+           const tutorialHover= page.getByRole("link",{name:"Tutorials"}).first();
+           await tutorialHover.hover();
+           await page.waitForTimeout(1_000) 
+        })
+        
+        })
+    })
+
+    test("Keyboard operations", async({page})=>{
+        await page.setViewportSize({width:2000,height:1080})
+        await page.goto("https://sqatools.in/automation-practice-page/");
+        const userNameField = page.getByPlaceholder("Enter username");
+        await userNameField.fill("user1@gmail.com")
+        await userNameField.press("Control+A");
+        await userNameField.press("Control+C")
+        const addressField = page.locator("#address")
+        await addressField.clear();
+        await addressField.press("Control+V");
+    });
 
 })
