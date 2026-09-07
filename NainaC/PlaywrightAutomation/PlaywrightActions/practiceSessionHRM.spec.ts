f688dc07bfac685b610672b74f16a43b56b8ff54
Nayana <nayanachide@gmail.com>
2026-09-07 19:49:25 +0530

    new file

---
+++ NainaC/PlaywrightAutomation/PlaywrightActions/practiceSessionHRM.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from '@playwright/test/'
+
+test.describe("Login to the HRM  Website", ()=>{
+test("login to the username and password", async({page}) =>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+await page.getByPlaceholder("Username").fill("Admin");
+await page.getByPlaceholder("Password").fill("admin123");
+ await page.getByRole('button', { name: 'Login' }).click();
+});
+});
